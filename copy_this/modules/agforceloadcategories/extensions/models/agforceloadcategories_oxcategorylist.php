6e9f8e4720c099a8a9df96b91385793415eb94e1
alexander <coding@aggrosoft.de>
2020-02-17 12:15:43 +0100

    initial version

---
+++ copy_this/modules/agforceloadcategories/extensions/models/agforceloadcategories_oxcategorylist.php
@@ -0,0 +1,12 @@
+<?php
+
+class agforceloadcategories_oxcategorylist extends agforceloadcategories_oxcategorylist_parent {
+
+    /**
+     * Force load full as default
+     *
+     * @var boolean
+     */
+    protected $_blForceFull = true;
+
+}
---
+++ copy_this/modules/agforceloadcategories/metadata.php
@@ -0,0 +1,27 @@
+<?php
+$aModule = array(
+    'id'           => 'agforceloadcategories',
+    'title'        => 'Force load all categories',
+    'description'  => 'Load full category tree in frontend',
+    'thumbnail'    => '',
+    'version'      => '1.0.0',
+    'author'       => 'Aggrosoft GmbH',
+    'extend'      => array(
+        'oxcategorylist' => 'agforceloadcategories/extensions/models/agforceloadcategories_oxcategorylist'
+    ),
+    'files'       => array(
+
+    ),
+    'templates'   => array(
+
+    ),
+    'events'       => array(
+
+    ),
+    'settings' => array(
+
+    ),
+    'blocks' => array(
+
+    )
+);
